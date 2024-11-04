fc7a8565239c74394e895f6de308cd60ebcf1746
Master_Laplace <guillaume.papineau@epitech.eu>
2024-11-03 19:02:58 -0500

    refactor(vk-wrapper): update copyright information and adjust include directives

--- src/plugin/vk-wrapper/shaderModule/ShaderModule.hpp
+++ src/plugin/vk-wrapper/shaderModule/ShaderModule.hpp
@@ -27,7 +27,11 @@
 
 #include "DebugMessenger.hpp"
 
-#define SHADER_DIR PROJECT_SOURCE_DIR + "shaders/"
+#ifdef PROJECT_SOURCE_DIR
+#    define SHADER_DIR PROJECT_SOURCE_DIR + "shaders/"
+#else
+#    define SHADER_DIR ""
+#endif
 
 namespace ES::Plugin::Wrapper {
 
