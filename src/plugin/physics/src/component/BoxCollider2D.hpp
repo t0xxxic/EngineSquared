8cf94d4943ba7950d188aba0fb87c1cc2f3dcd2e
MasterLaplace <guillaume.papineau@epitech.eu>
2024-11-28 06:34:05 -0500

    feat(BoxCollider2D): add JSON serialization and deserialization methods

--- src/plugin/physics/src/component/BoxCollider2D.hpp
+++ src/plugin/physics/src/component/BoxCollider2D.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <nlohmann/json.hpp>
 
 namespace ES::Plugin::Physics::Component {
 
@@ -12,5 +13,29 @@ struct BoxCollider2D {
      * The size of the box
      */
     glm::vec2 size;
+
+    /**
+     * Serialize the BoxCollider2D to JSON
+     */
+    nlohmann::json serialize() const
+    {
+        return nlohmann::json{
+            {"size", {size.x, size.y}}
+        };
+    }
+
+    /**
+     * Deserialize a BoxCollider2D from JSON
+     */
+    static BoxCollider2D deserialize(const nlohmann::json &json)
+    {
+        if (!json.contains("size") || !json["size"].is_array() || json["size"].size() != 2 ||
+            !json["size"][0].is_number() || !json["size"][1].is_number())
+            throw std::invalid_argument("Invalid JSON for BoxCollider2D deserialization");
+
+        glm::vec2 size{json["size"][0].get<float>(), json["size"][1].get<float>()};
+
+        return BoxCollider2D{size};
+    }
 };
 } // namespace ES::Plugin::Physics::Component
