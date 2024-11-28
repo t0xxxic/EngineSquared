ea6f088b33c63dba3bef5187cf5673d5c0419d73
MasterLaplace <guillaume.papineau@epitech.eu>
2024-11-28 06:31:14 -0500

    feat(MeshID): add JSON serialization and deserialization methods

--- src/plugin/object/src/component/MeshID.hpp
+++ src/plugin/object/src/component/MeshID.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "AssetID.hpp"
+#include <nlohmann/json.hpp>
 
 namespace ES::Plugin::Object::Component {
 /**
@@ -12,5 +13,28 @@ struct MeshID {
      * Id refering to a mesh.
      */
     ES::Plugin::Object::Utils::AssetID meshID;
+
+    /**
+     * Serialize the MeshID to JSON
+     */
+    nlohmann::json serialize() const
+    {
+        return nlohmann::json{
+            {"meshID", meshID}
+        };
+    }
+
+    /**
+     * Deserialize a MeshID from JSON
+     */
+    static MeshID deserialize(const nlohmann::json &json)
+    {
+        if (!json.contains("meshID") || !json["meshID"].is_number())
+            throw std::invalid_argument("Invalid JSON for MeshID deserialization");
+
+        Object::Utils::AssetID meshID = json["meshID"].get<Object::Utils::AssetID>();
+
+        return MeshID{meshID};
+    }
 };
 } // namespace ES::Plugin::Object::Component
